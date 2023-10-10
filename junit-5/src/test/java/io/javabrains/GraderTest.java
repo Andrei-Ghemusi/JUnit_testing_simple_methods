b8bcf8659b7a0cab64ba99ef00d13a84d178092a
Andrei-Ghemusi <andreighemusi@gmail.com>
2023-10-10 04:09:19 +0300

    changed some methods

--- junit-5/src/test/java/io/javabrains/GraderTest.java
+++ junit-5/src/test/java/io/javabrains/GraderTest.java
@@ -1,80 +1,96 @@
 package io.javabrains;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DisplayName("Running Grader")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GraderTest {
 
-    @Test
-    void fiftyNineShouldReturnF(){
-        var grader = new Grader();
-        assertEquals('F', grader.determineLetterGrade(59));
+    Grader grader;
+    TestInfo testInfo;
+    TestReporter testReporter;
+
+    @BeforeEach
+    void init(TestInfo testInfo, TestReporter testReporter){
+        this.testInfo =testInfo;
+        this.testReporter =testReporter;
+        grader = new Grader();
+        testReporter.publishEntry(format("Running %s with tags %s ", testInfo.getDisplayName(), testInfo.getTags()));
     }
 
-    @Test
-    void sixtyNineShouldReturnD(){
-        var grader = new Grader();
-        assertEquals('D', grader.determineLetterGrade(69));
+    @Nested
+    @DisplayName("First tests")
+    @Tag("First")
+    class EndInNine {
+
+        @Test
+        void fiftyNineShouldReturnF(){
+            assertEquals('F', grader.determineLetterGrade(59));
+        }
+
+        @Test
+        void sixtyNineShouldReturnD(){
+            assertEquals('D', grader.determineLetterGrade(69));
+        }
+
+        @Test
+        void seventyNineShouldReturnC(){
+            assertEquals('C', grader.determineLetterGrade(79));
+        }
+
+        @Test
+        void eightyNineShouldReturnB(){
+            assertEquals('B', grader.determineLetterGrade(89));
+        }
+
+        @Test
+        void ninetyNineShouldReturnA(){
+            assertEquals('A', grader.determineLetterGrade(99));
+        }
+
     }
 
-    @Test
-    void seventyNineShouldReturnC(){
-        var grader = new Grader();
-        assertEquals('C', grader.determineLetterGrade(79));
-    }
+    @Nested
+    @Tag("Second")
+    @DisplayName("Edge tests")
+    class EndingInZero{
 
-    @Test
-    void eightyNineShouldReturnB(){
-        var grader = new Grader();
-        assertEquals('B', grader.determineLetterGrade(89));
-    }
+        @Test
+        void zeroShouldReturnF() {
+            assertEquals('F', grader.determineLetterGrade(0));
+        }
 
-    @Test
-    void ninetyNineShouldReturnA(){
-        var grader = new Grader();
-        assertEquals('A', grader.determineLetterGrade(99));
-    }
+        @Test
+        void sixtyShouldReturnD() {
+            assertEquals('D', grader.determineLetterGrade(60));
+        }
 
+        @Test
+        void seventyShouldReturnC() {
+            assertEquals('C', grader.determineLetterGrade(70));
+        }
 
+        @Test
+        void eightyShouldReturnB(){
+            assertEquals('B', grader.determineLetterGrade(80));
+        }
 
+        @Test
+        void ninetyShouldReturnA() {
+            assertEquals('A', grader.determineLetterGrade(90));
+        }
 
-    @Test
-    void zeroShouldReturnF() {
-        var grader = new Grader();
-        assertEquals('F', grader.determineLetterGrade(0));
-    }
+        @Test
+git        void negativeOneShouldReturnIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class,
+                    ()-> { // lambda here
+                        grader.determineLetterGrade(-1);
+                    });
+        }
 
-    @Test
-    void sixtyShouldReturnD() {
-        var grader = new Grader();
-        assertEquals('D', grader.determineLetterGrade(60));
-    }
-
-    @Test
-    void seventyShouldReturnC() {
-        var grader = new Grader();
-        assertEquals('C', grader.determineLetterGrade(70));
-    }
-
-    @Test
-    void eightyShouldReturnB(){
-        var grader = new Grader();
-        assertEquals('B', grader.determineLetterGrade(80));
-    }
-
-    @Test
-    void ninetyShouldReturnA() {
-        var grader = new Grader();
-        assertEquals('A', grader.determineLetterGrade(90));
-    }
-
-    @Test
-    void negativeOneShouldReturnIllegalArgumentException() {
-        var grader = new Grader();
-        assertThrows(IllegalArgumentException.class,
-                ()-> { // lambda here
-                    grader.determineLetterGrade(-1);
-                });
     }
 }
